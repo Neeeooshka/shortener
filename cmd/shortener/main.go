8cfd736f4549402a1ba5e2de388ad76adaba3d6c
UkraintsevDV <neooshka@gmail.com>
2024-03-11 15:33:06 +0300

    create HTTP server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,57 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"regexp"
+)
+
+func main() {
+	if err := runHTTPServer(); err != nil {
+		panic(err)
+	}
+}
+
+// run HTTP server
+func runHTTPServer() error {
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/", HTTPHandler)
+	err := http.ListenAndServe(`:8080`, serverMux)
+	return err
+}
+
+func HTTPHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Header.Get("Content-Type") != "text/plain" && r.Header.Get("Content-Type") != "" {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+
+	switch r.Method {
+	case http.MethodPost:
+		endPointPOST(w, r)
+	case http.MethodGet:
+		endPointGET(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+// обработчик HTTP-запроса POST
+func endPointPOST(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`method POST`))
+}
+
+// обработчик HTTP-запроса GET
+func endPointGET(w http.ResponseWriter, r *http.Request) {
+
+	if match, err := regexp.MatchString("[A-Za-z0-9]", r.URL.String()); err != nil || !match {
+		http.Error(w, "link is incorrect", http.StatusBadRequest)
+	}
+
+	w.Write([]byte(`method GET`))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Neeeooshka/alice-skill.git
+
+go 1.21.6
