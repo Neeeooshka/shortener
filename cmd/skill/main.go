e00435ba1b8c36558591bb19728b9852c7f63956
UkraintsevDV <neooshka@gmail.com>
2024-04-12 16:13:41 +0300

    add alice-skill compression

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -7,6 +7,7 @@ import (
 	"go.uber.org/zap"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -18,5 +19,44 @@ func main() {
 	}
 
 	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
-	log.Fatal(http.ListenAndServe(flagRunAddr, logger.RequestLogger(handlers.AliceSkill)))
+	// оборачиваем хендлер в middleware с логированием и поддержкой gzip
+	log.Fatal(http.ListenAndServe(flagRunAddr, logger.RequestLogger(gzipMiddleware(handlers.AliceSkill))))
+}
+
+func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// по умолчанию устанавливаем оригинальный http.ResponseWriter как тот,
+		// который будем передавать следующей функции
+		ow := w
+
+		// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		if supportsGzip {
+			// оборачиваем оригинальный http.ResponseWriter новым с поддержкой сжатия
+			cw := newCompressWriter(w)
+			// меняем оригинальный http.ResponseWriter на новый
+			ow = cw
+			// не забываем отправить клиенту все сжатые данные после завершения middleware
+			defer cw.Close()
+		}
+
+		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
+		contentEncoding := r.Header.Get("Content-Encoding")
+		sendsGzip := strings.Contains(contentEncoding, "gzip")
+		if sendsGzip {
+			// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
+			cr, err := newCompressReader(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			// меняем тело запроса на новое
+			r.Body = cr
+			defer cr.Close()
+		}
+
+		// передаём управление хендлеру
+		h.ServeHTTP(ow, r)
+	}
 }
